bc5a3e4ec66450f775a242b79d452298cde1b2ba
ALAN <bryanbaez2504@gmail.com>
2020-08-25 10:52:52 -0700

    Trabajos y proyectos realizados en JS

---
+++ js_sandbox/js_curso/js/dom.js
@@ -0,0 +1,56 @@
+/*
+  DOM:
+      Nos permite manejar html con js a traves
+      de los navegadores
+*/
+
+// console.log("********** Elementos del documento **********");
+// console.log(window.document);
+// console.log(document);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.doctype);
+// console.log(document.charset);
+// console.log(document.title);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.forms);
+// console.log(document.styleSheets);
+// console.log(document.scripts);
+// setTimeout(() => {
+//   console.log(document.getSelection().toString());
+// }, 3000);
+// document.write('<h2>Hola Mundo desde el DOM</h2>');
+
+
+
+
+
+// console.log("********** Nodos, Elementos y Sectores **********");
+
+// // Ya no se utilizan
+// console.log(document.getElementsByTagName("li"));
+// console.log(document.getElementsByClassName("card"));
+// console.log(document.getElementsByName("nombre"));
+
+
+// console.log(document.getElementById("menu"));
+// console.log(document.querySelector("#menu"));
+// console.log(document.querySelector("a"));
+// console.log(document.querySelectorAll("a"));
+// console.log(document.querySelectorAll("a").length);
+// document.querySelectorAll('a').forEach(el => console.log(el));
+// console.log(document.querySelector(".card"));
+// console.log(document.querySelectorAll(".card"));
+// console.log(document.querySelectorAll(".card")[2]);
+// console.log(document.querySelectorAll("#menu li"));
+// console.log(document.querySelector("#menu li"));
+
+
+
+
+
+
+// **********  DOM: Atributos y Data-Attributes **********
+
