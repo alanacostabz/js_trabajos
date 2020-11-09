388893b042b67e924d8a6bbd0c60803e7fd421a5
Bryan Acosta <bryanbaez@live.com.mx>
2020-11-09 13:39:22 -0700

    Trabajos y proyectos realizados en JS

--- js_sandbox/js_curso/js/index_dom.js
+++ js_sandbox/js_curso/js/index_dom.js
@@ -1,4 +1,5 @@
 import scrollTopButton from "./dom/boton-scroll.js";
+import slider from "./dom/carrusel.js";
 import countdown from "./dom/cuenta_regresiva.js";
 import userDeviceInfo from "./dom/deteccion_dispositivos.js";
 import networkStatus from "./dom/deteccion_red.js";
@@ -9,9 +10,13 @@ import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import responsiveMedia from "./dom/objeto_responsive.js";
 import responsiveTester from "./dom/prueba_responsive.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
+import scrollSpy from "./dom/scroll_espia.js";
 import draw from "./dom/sorteo.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
 import darkTheme from "./dom/tema-oscuro.js";
+import contactFormValidations from "./dom/validaciones_formulario.js";
+import smartVideo from "./dom/video_inteligente.js";
+import speechReader from "./narrador.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -38,6 +43,10 @@ d.addEventListener("DOMContentLoaded", (e) => {
   getGeolocation("geolocation");
   searchFilters(".card-filter", ".card");
   draw("#winner-btn", ".player");
+  slider();
+  scrollSpy();
+  smartVideo();
+  contactFormValidations();
 });
 
 d.addEventListener("keydown", (e) => {
@@ -47,3 +56,4 @@ d.addEventListener("keydown", (e) => {
 
 darkTheme(".dark-theme-btn", "dark-mode");
 networkStatus();
+speechReader();
